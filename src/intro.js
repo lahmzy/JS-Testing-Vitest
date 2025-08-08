f04cfffedc317959e641720e5da61d517a88a7d9
unknown <olusanya544@gmail.com>
2025-08-08 22:34:41 +0100

    added average test

--- src/intro.js
+++ src/intro.js
@@ -1,6 +1,6 @@
 // Lesson: Writing your first tests
 export function max(a, b) {
-  return ( a > b ) ? a : b;
+  return a > b ? a : b;
 }
 
 // Exercise
@@ -9,4 +9,12 @@ export function fizzBuzz(n) {
   if (n % 3 === 0) return "Fizz";
   if (n % 5 === 0) return "Buzz";
   return n.toString();
+}
+
+export function caluclateAverage(arr) {
+  if (arr.length === 0) return NaN;
+
+  const sum = arr.reduce((acc, current) => acc + current, 0);
+
+  return sum / arr.length;
 }
--- test/intro.test.js
+++ test/intro.test.js
@@ -1,35 +1,45 @@
-import { describe,it,expect,test } from "vitest";
-import { max } from "../src/intro.js";
-import { fizzBuzz } from "../src/intro.js";
+import { describe, expect, it } from "vitest";
+import { caluclateAverage, fizzBuzz, max } from "../src/intro.js";
 
 describe("max", () => {
-    it("should return first argument if its greater", () => {
-        expect(max(2, 1)).toBe(2);
-    });
+  it("should return first argument if its greater", () => {
+    expect(max(2, 1)).toBe(2);
+  });
 
-    it("should return the second argument if greater", () => {
-        expect(max(1,2)).toBe(2)
+  it("should return the second argument if greater", () => {
+    expect(max(1, 2)).toBe(2);
+  });
+
+  it("should return the first number if both are equal", () => {
+    expect(max(3, 3)).toBe(3);
+  });
+});
+
+describe("fizzbuzz", () => {
+  it("should return FizzBuzz if number is both divisible by 3 and 5", () => {
+    expect(fizzBuzz(15)).toBe("FizzBuzz");
+  });
+
+  it("should return Fizz if number is both divisible by 3", () => {
+    expect(fizzBuzz(81)).toBe("Fizz");
+  });
+
+  it("should return Buzz if number is both divisible by 5", () => {
+    expect(fizzBuzz(10)).toBe("Buzz");
+  });
+
+  it("should return a string if number is neither divisible by 5 nor 3", () => {
+    expect(fizzBuzz(8)).toBe("8");
+  });
+});
+
+describe("calculate Average", ()=> {
+    it("should return Nan if argument passed is an empty array", () => {
+        expect(caluclateAverage([])).toBeNaN
     })
-    
-    it("should return the first number if both are equal", () => {
-        expect(max(3, 3)).toBe(3);
-    });
+
+    it("shoulde return avergae if an array is passed", () => {
+        expect(caluclateAverage([1,2,3])).toBe(2)
+    })
 })
 
-describe('fizzbuzz', () => {
-  it("should return FizzBuzz if number is both divisible by 3 and 5",() => {
-    expect(fizzBuzz(15)).toBe("FizzBuzz")
-  })
-
-  it("should return Fizz if number is both divisible by 3",() => {
-    expect(fizzBuzz(81)).toBe("Fizz")
-  })
-
-   it("should return Buzz if number is both divisible by 5",() => {
-    expect(fizzBuzz(10)).toBe("Buzz")
-  })
-
-  it("should return a string if number is neither divisible by 5 nor 3",() => {
-    expect(fizzBuzz(8)).toBe("8")
-  })
-})
